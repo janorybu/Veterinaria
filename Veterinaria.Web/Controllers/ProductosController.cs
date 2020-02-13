9577c4abf32c4be0c9a9665a4a0c44832a07f454
itpc <itpc@DALYB>
2020-02-13 14:58:21 -0600

    agregando arquitectura basica

---
+++ Veterinaria.Web/Controllers/ProductosController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Veterinaria.Web.Models;
+
+namespace Veterinaria.Web.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: Productos
+        public ActionResult Index()
+        {
+
+            var producto1 = new ProductoModel();
+            producto1.Codigo = 1;
+            producto1.Descripcion = "Antibioticos";
+
+
+            var producto2 = new ProductoModel();
+            producto2.Codigo = 2;
+            producto2.Descripcion = "Antiinflamatorios";
+
+            var producto3 = new ProductoModel();
+            producto3.Codigo = 3;
+            producto3.Descripcion = "Vacunas";
+
+          
+            var listadeProductos = new List<ProductoModel>();
+            listadeProductos.Add(producto1);
+            listadeProductos.Add(producto2);
+            listadeProductos.Add(producto3);
+            
+
+            return View(listadeProductos);
+            
+           }
+    }
+}
---
+++ Veterinaria.Web/Models/ProductoModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Veterinaria.Web.Models
+{
+    public class ProductoModel
+    {
+        public int Codigo { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
